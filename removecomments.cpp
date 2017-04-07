031c837af88322b8bb110ae0479a039e8cd4aba1
ronaldroldanurrutia <ajedrez_11@hotmail.com>
2017-04-07 10:20:28 -0500

    Add files via upload

--- removecomments.cpp
+++ removecomments.cpp
@@ -1,29 +1,48 @@
 #include<iostream>
 #include<fstream>
+#include<vector>
 
 using namespace std;
 
 void removecomments(ifstream & is,ofstream & os){
-    string elemento;
-    //vector<>
-    //while(getline(is,elemento)){
-    //
-    //}
-
+    string elemento,elemento2="\\";
+    vector<string>v;
+    while(getline(is,elemento)){
+        v.push_back(elemento);
+    }
+    string elemento1="\\";
+    for(int i=0;i<v.size();i++){
+        bool verdad=true;
+        int j=0;
+        for(;j<(v[i].size()-1);){
+            if(v[i][j]!='/' && v[i][j+1]!='*'){
+                verdad = false;
+                j+=2;
+            }
+            if(verdad){
+                os <<  v[i][j] ;
+            }
+            if(v[i][j]==elemento1[0] && v[i][j+1]=='*'){
+                verdad = true;
+                j+=2;
+            }
+            if(v[i][j]=='/' && v[i][j+1]=='/'){
+                break;
+            }
+        }
+        if(verdad){
+            if(j==v[i].size()-1){
+                os << v[i][j];
+            }
+        }
+        os << endl;
+    }
 }
 
 int main(){
     ifstream archivo;
     string elemento;
     archivo.open("lista.txt");
-    while(getline(archivo,elemento)){
-        for(int i=0;i<elemento.size();i++){
-            if(elemento[i]==' '){
-                cout << 1;
-            }
-        }
-        cout << endl;
-    }
     cout << elemento << endl;
     return 0;
 
