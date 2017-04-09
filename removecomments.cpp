b88e6a97ee6c5beeb570e78d1850ebe30730b70e
ronaldroldanurrutia <ajedrez_11@hotmail.com>
2017-04-08 21:58:37 -0500

    Add files via upload

--- removecomments.cpp
+++ removecomments.cpp
@@ -1,38 +1,34 @@
 #include<iostream>
 #include<fstream>
-#include<vector>
 
 using namespace std;
 
-void removecomments(ifstream & is,ofstream & os){
-    string elemento,elemento2="\\";
-    vector<string>v;
-    while(getline(is,elemento)){
-        v.push_back(elemento);
-    }
-    string elemento1="\\";
-    for(int i=0;i<v.size();i++){
-        bool verdad=true;
+void removecomments(ifstream &is,ofstream &os){
+    string v;
+    bool verdad=true;
+    while(getline(is,v)){
         int j=0;
-        for(;j<(v[i].size()-1);){
-            if(v[i][j]!='/' && v[i][j+1]!='*'){
+        for(;j<v.size()-1;){
+            if(v[j]=='/' && v[j+1]=='*'){
                 verdad = false;
-                j+=2;
+                j+=1;
             }
-            if(verdad){
-                os <<  v[i][j] ;
-            }
-            if(v[i][j]==elemento1[0] && v[i][j+1]=='*'){
-                verdad = true;
-                j+=2;
-            }
-            if(v[i][j]=='/' && v[i][j+1]=='/'){
+            if(v[j]=='/' && v[j+1]=='/'){
+                j=-1;
                 break;
             }
+            if(verdad==true){
+                os << v[j];
+            }
+            if(v[j]=='*' && v[j+1]=='/'){
+                verdad = true;
+                j+=1;
+            }
+            j++;
         }
-        if(verdad){
-            if(j==v[i].size()-1){
-                os << v[i][j];
+        if(verdad==true){
+            if(j==v.size()-1){
+                os << v[j];
             }
         }
         os << endl;
@@ -41,9 +37,10 @@ void removecomments(ifstream & is,ofstream & os){
 
 int main(){
     ifstream archivo;
-    string elemento;
-    archivo.open("lista.txt");
-    cout << elemento << endl;
+    ofstream os;
+    archivo.open("hola.txt");
+    os.open("hola1.txt");
+    removecomments(archivo,os);
     return 0;
-
 }
+
